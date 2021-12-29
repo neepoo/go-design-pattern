eccfff086f7a1ddd4095967648bb6f9f425fe8b5
韦智凯 <neepoowzk@gmail.com>
2021-12-29 15:57:01 +0800

    中介模式,说实话没看懂书中的例子,有点抽象

---
+++ behavior/mediator/doc.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+Let's continue with the Mediator pattern. As its name implies, it's a pattern that will be in
+between two types to exchange information. But, why will we want this behavior? Let's see
+it in detail
+
+exchange message between two type.
+
+// 设计模式一个重要的目标就是解偶
+With design patterns, one of the key objectives of any of them is to avoid tight coupling
+between objects.
+
+Acceptance criteria
+It sounds quite funny to talk about acceptance criteria to define a calculator but, let's do it
+anyway.
+1. Define an operation called Sum that takes a number and adds it to another
+number.
+2. Define an operation called Subtract that takes a number and adds it to another
+number.
+3. Well, I don't know you but I really need a rest after this. So why are we defining
+this so much? Patience, we will see it soon.
+*/
